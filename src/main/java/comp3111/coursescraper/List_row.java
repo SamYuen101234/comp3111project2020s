5383cfe957d18fa54f753235bb304d4ea8c5847f
SamYuen101234 <samyuan101234@gmail.com>
2020-04-24 14:02:25 +0800

    Change task 2 and fix its bugs Change task 3 and fix its bugs modify the class list_row

--- src/main/java/comp3111/coursescraper/List_row.java
+++ src/main/java/comp3111/coursescraper/List_row.java
@@ -1,13 +1,35 @@
 package comp3111.coursescraper;
 
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Set;
+import javafx.scene.control.CheckBox;
+import javafx.fxml.FXML;
+import javafx.event.EventHandler;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.event.ActionEvent;
 
 public class List_row {
 	private String course_code;
 	private String section;
 	private String course_name;
 	private String instructor;
+	private Slot[] slot;
+	//private int day; // Monday = 0, Tuesday = 1; ... Saturday = 5
+	//private LocalTime start;
+	//private LocalTime end;
+	private boolean select;
+	
+	public List_row() {
+		this.course_code = null;
+		this.course_name = null;
+		//this.day = -1;
+		//this.start = null;
+		//this.end = null;
+		this.select = false;
+		
+	}
 	
 	public List_row(Course courses, Section section, Slot slot) {
 		String course_title = courses.getTitle();
@@ -32,23 +54,105 @@ public class List_row {
 		this.section = section.getSectionID();
 		Set<String> instructors = slot.getAllInstructor();
 		this.instructor = instructors.toString();
+		//this.day = slot.getDay();
+		//this.start = slot.getStart();
+		//this.end = slot.getEnd();
+		//this.select = false;
+	}
+	
+	@Override
+	public List_row clone() {
+		List_row new_class = new List_row();
+		new_class.course_code = this.course_code;
+		new_class.course_name = this.course_name;
+		//new_class.day = this.day;
+		//new_class.end = this.end;
+		//new_class.instructor = this.instructor;
+		//new_class.section = this.section;
+		//new_class.start = this.start;
+		new_class.select = this.select;
+		return new_class;
 	}
 	
 	public String getCourse_code() {
 		return this.course_code;
 	}
 	
+	public void setCourse_code(String course_code) {
+		this.course_code = course_code;
+	}
+	
 	public String getSection() {
 		return this.section;
+	}
+	
+	public void setSection(String section) {
+		this.section = section;
 	}
 	
 	public String getCourse_name() {
 		return this.course_name;
 	}
 	
+	public void setCourse_name(String course_name) {
+		this.course_name = course_name;
+	}
+	
 	public String getInstructor() {
 		return this.instructor;
 	}
+	
+	public void set_Instructor(String instructor) {
+		this.instructor = instructor;
+	}
+	
+	/*public int getDay() {
+		return day;
+	}
+	
+	public void setDay(int day) {
+		this.day = day;
+	}
+	
+	public LocalTime getStart() {
+		return start;
+	}
+	
+	public void setStart(LocalTime start) {
+		this.start = start;
+	}
+	
+	public int getStartHour() {
+		return start.getHour();
+	}
+	
+	public int getStartMinute() {
+		return start.getMinute();
+	}
+	
+	public LocalTime getEnd() {
+		return end;
+	}
+	
+	public void setEnd(LocalTime end) {
+		this.end = end;
+	}
+	
+	public int getEndHour() {
+		return end.getHour();
+	}
+	public int getEndMinute() {
+		return end.getMinute();
+	}*/
+	
+	public boolean getSelect() {
+		return select;
+	}
+	
+	public void setSelect(boolean select) {
+		this.select = select;
+	}
+	
 	
 	
 }
