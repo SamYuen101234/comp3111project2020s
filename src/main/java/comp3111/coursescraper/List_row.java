339830d25c861b1429fecddc23320f0317e03fc1
SamYuen101234 <samyuan101234@gmail.com>
2020-04-25 02:43:22 +0800

    the version 1 on task 3 (can enroll course and show it on console now)

--- src/main/java/comp3111/coursescraper/List_row.java
+++ src/main/java/comp3111/coursescraper/List_row.java
@@ -1,37 +1,26 @@
 package comp3111.coursescraper;
 
-import java.time.LocalTime;
-import java.util.List;
-import java.util.Set;
-import javafx.scene.control.CheckBox;
-import javafx.fxml.FXML;
-import javafx.event.EventHandler;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.event.ActionEvent;
-
 public class List_row {
 	private String course_code;
 	private String section;
 	private String course_name;
+	private String course_title;
 	private String instructor;
-	private Slot[] slot;
-	//private int day; // Monday = 0, Tuesday = 1; ... Saturday = 5
-	//private LocalTime start;
-	//private LocalTime end;
-	private boolean select;
+	private Slot[] slots;
+	private int numSlots;
+	private String section_type;
+	private boolean select = false;
 	
 	public List_row() {
 		this.course_code = null;
 		this.course_name = null;
-		//this.day = -1;
-		//this.start = null;
-		//this.end = null;
-		this.select = false;
-		
+		this.section = null;
+		this.instructor = null;
+		this.slots = null;
+		this.numSlots = -1;
 	}
 	
-	public List_row(Course courses, Section section, Slot slot) {
+	public List_row(Course courses, Section section) {
 		String course_title = courses.getTitle();
 		String temp = new String(); int counter = 0;
 		for(char ch: course_title.toCharArray()) {
@@ -51,28 +40,38 @@ public class List_row {
 			temp += ch;
 		}
 		this.course_name = temp;
+		this.course_title = courses.getTitle();
 		this.section = section.getSectionID();
-		Set<String> instructors = slot.getAllInstructor();
-		this.instructor = instructors.toString();
-		//this.day = slot.getDay();
-		//this.start = slot.getStart();
-		//this.end = slot.getEnd();
-		//this.select = false;
+		this.instructor = section.getAllInstructor().toString();
+		this.numSlots = section.getNumSlots();
+		slots = new Slot[this.numSlots];
+		for(int i = 0; i < this.numSlots; ++i) {
+			slots[i] = section.getSlot(i).clone();
+		}
+		String first2char = this.section.substring(0, 2);
+		String firstchar = this.section.substring(0, 1);
+		if(first2char.contentEquals("LA"))
+			this.section_type = first2char;
+		else
+			this.section_type = firstchar;
 	}
 	
-	@Override
+	/*@Override
 	public List_row clone() {
 		List_row new_class = new List_row();
 		new_class.course_code = this.course_code;
 		new_class.course_name = this.course_name;
-		//new_class.day = this.day;
-		//new_class.end = this.end;
-		//new_class.instructor = this.instructor;
-		//new_class.section = this.section;
-		//new_class.start = this.start;
+		new_class.instructor = this.instructor;
+		new_class.section = this.section;
 		new_class.select = this.select;
+		new_class.course_title = this.course_title;
+		new_class.numSlots = this.numSlots;
+		new_class.slots = new Slot[new_class.numSlots];
+		for(int i = 0; i < slots.length; ++i) {
+			new_class.slots[i] = this.slots[i].clone();
+		}
 		return new_class;
-	}
+	}*/
 	
 	public String getCourse_code() {
 		return this.course_code;
@@ -106,45 +105,7 @@ public class List_row {
 		this.instructor = instructor;
 	}
 	
-	/*public int getDay() {
-		return day;
-	}
-	
-	public void setDay(int day) {
-		this.day = day;
-	}
-	
-	public LocalTime getStart() {
-		return start;
-	}
-	
-	public void setStart(LocalTime start) {
-		this.start = start;
-	}
-	
-	public int getStartHour() {
-		return start.getHour();
-	}
-	
-	public int getStartMinute() {
-		return start.getMinute();
-	}
-	
-	public LocalTime getEnd() {
-		return end;
-	}
-	
-	public void setEnd(LocalTime end) {
-		this.end = end;
-	}
-	
-	public int getEndHour() {
-		return end.getHour();
-	}
-	public int getEndMinute() {
-		return end.getMinute();
-	}*/
-	
+		
 	public boolean getSelect() {
 		return select;
 	}
@@ -153,6 +114,22 @@ public class List_row {
 		this.select = select;
 	}
 	
+	public String getTitle() {
+		return this.course_title;
+	}
+	
+	public int getNumSlot() {
+		return this.numSlots;
+	}
+	
+	public Slot[] getSlot() {
+		return this.slots;
+	}
+	
+	public String getSectionType() {
+		return this.section_type;
+	}
+
 	
 	
 }
